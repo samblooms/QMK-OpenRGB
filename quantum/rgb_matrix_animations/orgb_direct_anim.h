0b95290523d5c4f26fbffbb2d6f74c12b9ac7977
Kasper <ofek4430@gmail.com>
2020-11-16 21:10:03 +0200

    Update 'HID_DIRECT' name to 'ORGB_DIRECT' to match the new naming

--- quantum/rgb_matrix_animations/orgb_direct_anim.h
+++ quantum/rgb_matrix_animations/orgb_direct_anim.h
@@ -1,8 +1,8 @@
 #ifdef ORGB_ENABLE
-RGB_MATRIX_EFFECT(HID_DIRECT)
+RGB_MATRIX_EFFECT(ORGB_DIRECT)
 #    ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
-bool HID_DIRECT(effect_params_t* params) {
+bool ORGB_DIRECT(effect_params_t* params) {
     RGB_MATRIX_USE_LIMITS(led_min, led_max);
 
     for (uint8_t i = led_min; i < led_max; i++) {
